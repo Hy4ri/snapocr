606a01accee48b73de58d9a881cd0af7bc001572
M57 <41645758+Hy4ri@users.noreply.github.com>
2026-08-16 10:41:03 +0300

    chore(release): bump version to v1.0.0 (#5)

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
       packages = forAllSystems (pkgs: {
         default = pkgs.rustPlatform.buildRustPackage {
           pname = "snapocr";
-          version = "0.3.0";
+          version = "1.0.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
           nativeBuildInputs = with pkgs; [ pkg-config makeWrapper ];
